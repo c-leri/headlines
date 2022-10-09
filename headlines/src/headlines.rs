3464d523532f4a31d9a26b2cf59cba434ca79ff7
celeri <celestin.bouchet@gmail.com>
2022-10-10 00:48:41 +0200

    7b. Building a GUI app in Rust [Part B] (https://youtu.be/SvFPdgGwzTQ)

--- headlines/src/headlines.rs
+++ headlines/src/headlines.rs
@@ -1,10 +1,40 @@
 use std::borrow::Cow;
-use eframe::egui::{Button, CentralPanel, Color32, CtxRef, FontDefinitions, FontFamily, Hyperlink, Label, Layout, menu, ScrollArea, Separator, TextStyle, TopBottomPanel, Ui};
+use std::sync::mpsc::{channel, Receiver, Sender, sync_channel, SyncSender};
+use std::thread;
+use eframe::egui::{Button, CentralPanel, Color32, CtxRef, FontDefinitions, FontFamily, Hyperlink, Key, Label, Layout, menu, ScrollArea, Separator, TextStyle, TopBottomPanel, Ui, Visuals, Window};
 use eframe::epi::{App, Frame, Storage};
+use serde::{Serialize, Deserialize};
+use newsapi::NewsAPI;
 
 const PADDING: f32 = 5.;
 const WHITE: Color32 = Color32::from_rgb(255, 255, 255);
+const BLACK: Color32 = Color32::from_rgb(0,0,0);
 const CYAN: Color32 = Color32::from_rgb(0, 255, 255);
+const RED: Color32 = Color32::from_rgb(255,0,0);
+
+enum Msg
+{
+    APIKeySet(String)
+}
+
+#[derive(Serialize, Deserialize)]
+struct HeadlinesConfig
+{
+    dark_mode: bool,
+    api_key: String
+}
+
+impl Default for HeadlinesConfig
+{
+    fn default() -> Self
+    {
+        Self
+        {
+            dark_mode: true,
+            api_key: String::new()
+        }
+    }
+}
 
 struct NewsCardData
 {
@@ -15,25 +45,26 @@ struct NewsCardData
 
 pub struct Headlines
 {
-    articles: Vec<NewsCardData>
+    articles: Vec<NewsCardData>,
+    config: HeadlinesConfig,
+    api_key_initialized: bool,
+    news_rx: Option<Receiver<NewsCardData>>,
+    app_tx: Option<SyncSender<Msg>>
 }
 
 impl Headlines
 {
     pub fn new() -> Self
     {
-        let iter = (0..20).map(
-            |a| NewsCardData
-            {
-                title: format!("title{}", a),
-                desc: format!("desc{}", a),
-                url: format!("https://example.com/{}", a)
-            }
-        );
+        let config: HeadlinesConfig = confy::load("headlines").unwrap_or_default();
 
         Self
         {
-            articles: Vec::from_iter(iter)
+            articles: Vec::new(),
+            api_key_initialized: !config.api_key.is_empty(),
+            config,
+            news_rx: None,
+            app_tx: None
         }
     }
 
@@ -69,31 +100,45 @@ impl Headlines
 
     fn render_news_cards(&self, ui: &mut Ui)
     {
-        for a in &self.articles
+        if self.articles.is_empty()
         {
-            // title
-            ui.add_space(PADDING);
-            let title = format!("▶ {}", a.title);
-            ui.colored_label(WHITE, title);
+            ui.vertical_centered
+            (
+                |ui|
+                    {
+                        ui.label("Loading...");
+                    }
+            );
+        }
+        else {
+            for a in &self.articles
+            {
+                // title
+                ui.add_space(PADDING);
+                let title = format!("▶ {}", a.title);
+                if self.config.dark_mode
+                { ui.colored_label(WHITE, title); } else { ui.colored_label(BLACK, title); }
 
-            // desc
-            ui.add_space(PADDING);
-            let desc = Label::new(&a.desc).text_style(TextStyle::Button);
-            ui.add(desc);
+                // desc
+                ui.add_space(PADDING);
+                let desc = Label::new(&a.desc).text_style(TextStyle::Button);
+                ui.add(desc);
 
-            // links
-            ui.style_mut().visuals.hyperlink_color = CYAN;
-            ui.add_space(PADDING);
-            ui.with_layout(Layout::right_to_left(), |ui| {
-                ui.add(Hyperlink::new(&a.url).text("read more ⤴"));
-            });
+                // links
+                if self.config.dark_mode
+                { ui.style_mut().visuals.hyperlink_color = CYAN; } else { ui.style_mut().visuals.hyperlink_color = RED; }
+                ui.add_space(PADDING);
+                ui.with_layout(Layout::right_to_left(), |ui| {
+                    ui.add(Hyperlink::new(&a.url).text("read more ⤴"));
+                });
 
-            ui.add_space(PADDING);
-            ui.add(Separator::default());
+                ui.add_space(PADDING);
+                ui.add(Separator::default());
+            }
         }
     }
 
-    fn render_top_panel(&self, ctx: &CtxRef)
+    fn render_top_panel(&mut self, ctx: &CtxRef, frame: &mut Frame<'_>)
     {
         TopBottomPanel::top("top_panel").show(ctx,
             |ui|
@@ -112,8 +157,39 @@ impl Headlines
                             |ui|
                             {
                                 let close_btn = ui.add(Button::new("❌").text_style(TextStyle::Body));
+                                if close_btn.clicked()
+                                {
+                                    frame.quit();
+                                }
+
                                 let refresh_btn = ui.add(Button::new("🔄").text_style(TextStyle::Body));
-                                let theme_btn = ui.add(Button::new("🌙").text_style(TextStyle::Body));
+                                if refresh_btn.clicked()
+                                {
+                                    self.articles.clear();
+
+                                    let (mut news_tx, news_rx) = channel();
+                                    self.news_rx = Some(news_rx);
+
+                                    let api_key = self.config.api_key.to_string();
+                                    thread::spawn(
+                                        move ||
+                                        {
+                                            fetch_news(&api_key, &mut news_tx);
+                                        }
+                                    );
+                                }
+
+                                let theme_btn = ui
+                                    .add(Button::new({
+                                        if self.config.dark_mode
+                                        { "🌞" }
+                                        else
+                                        { "🌙" }
+                                    }).text_style(TextStyle::Body));
+                                if theme_btn.clicked()
+                                {
+                                    self.config.dark_mode = !self.config.dark_mode;
+                                }
                             }
                         );
                     }
@@ -122,26 +198,124 @@ impl Headlines
             }
         );
     }
-}
 
-impl App for Headlines
-{
-    fn update(&mut self, ctx: &CtxRef, _frame: &mut Frame<'_>) {
-        self.render_top_panel(ctx);
-        CentralPanel::default().show(ctx,
+    fn render_config(&mut self, ctx: &CtxRef)
+    {
+        Window::new("Configuration").show(ctx,
             |ui|
             {
-                render_header(ui);
-                ScrollArea::auto_sized().show(ui,
-                    |ui| { self.render_news_cards(ui); }
-                );
-                render_footer(ctx);
+                ui.label("Enter your API key for newsapi.org");
+                let text_input = ui.text_edit_singleline(&mut self.config.api_key);
+                if text_input.lost_focus() && ui.input().key_pressed(Key::Enter)
+                {
+                    if let Err(e) = confy::store("headlines", HeadlinesConfig
+                    {
+                        dark_mode: self.config.dark_mode,
+                        api_key: self.config.api_key.to_string()
+                    })
+                    {
+                        tracing::error!("Failed saving app state: {}", e);
+                    }
+                    self.api_key_initialized = true;
+                    if let Some(tx) = &self.app_tx
+                    {
+                        if let Err(e) = tx.send(Msg::APIKeySet(self.config.api_key.to_string()))
+                        {
+                            tracing::error!("Failed sending msg: {}", e)
+                        }
+                    }
+                    tracing::error!("API key set");
+                }
+                ui.label("If you haven't registered for the API key, head over to");
+                ui.hyperlink("https://newsapi.org");
             }
         );
     }
 
+    fn preload_articles(&mut self)
+    {
+        if let Some(rx) = &self.news_rx
+        {
+            match rx.try_recv()
+            {
+                Ok(news_data) => {
+                    self.articles.push(news_data);
+                }
+                Err(e) => {
+                    tracing::warn!("Error receiving msg: {}", e);
+                }
+            }
+        }
+    }
+}
+
+impl App for Headlines
+{
+    fn update(&mut self, ctx: &CtxRef, frame: &mut Frame<'_>) {
+        ctx.request_repaint();
+
+        if self.config.dark_mode
+        {
+            ctx.set_visuals(Visuals::dark());
+        }
+        else
+        {
+            ctx.set_visuals(Visuals::light());
+        }
+
+        if !self.api_key_initialized
+        { self.render_config(ctx); }
+        else {
+            self.preload_articles();
+
+            self.render_top_panel(ctx, frame);
+            CentralPanel::default().show(ctx,
+                |ui|
+                {
+                    render_header(ui);
+                    ScrollArea::auto_sized().show(ui,
+                        |ui|
+                        { self.render_news_cards(ui); }
+                    );
+                    render_footer(ctx);
+                }
+            );
+        }
+    }
+
     fn setup(&mut self, ctx: &CtxRef, _frame: &mut Frame<'_>, _storage: Option<&dyn Storage>)
     {
+        let api_key = self.config.api_key.to_string();
+
+        let (mut news_tx, news_rx) = channel();
+        self.news_rx = Some(news_rx);
+
+        let (app_tx, app_rx) = sync_channel(1);
+        self.app_tx = Some(app_tx);
+
+        thread::spawn(
+            move ||
+            {
+                if !api_key.is_empty()
+                { fetch_news(&api_key, &mut news_tx); }
+                else
+                {
+                    loop
+                    {
+                        match app_rx.recv()
+                        {
+                            Ok(Msg::APIKeySet(api_key)) => {
+                                fetch_news(&api_key, &mut news_tx);
+                            }
+                            Err(e) => {
+                                tracing::error!("Failed receiving msg: {}", e);
+                            }
+                        }
+                    }
+                }
+            }
+        );
+
         self.configure_fonts(ctx);
     }
 
@@ -197,4 +371,28 @@ fn render_footer(ctx: &CtxRef)
             );
         }
     );
+}
+
+fn fetch_news(api_key: &str, news_tx: &mut Sender<NewsCardData>)
+{
+    if let Ok(response) = NewsAPI::new(&api_key).fetch()
+    {
+        for article in response.articles()
+        {
+            let news = NewsCardData
+            {
+                title: article.title().to_string(),
+                desc: article.description().map(|s| s.to_string()).unwrap_or("...".to_string()),
+                url: article.url().to_string()
+            };
+            if let Err(e) = news_tx.send(news)
+            {
+                tracing::error!("Error sending news data: {}", e);
+            }
+        }
+    }
+    else
+    {
+        tracing::error!("Failed fetching news");
+    }
 }
--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -5,8 +5,11 @@ use eframe::{NativeOptions, run_native};
 use headlines::Headlines;
 
 fn main() {
+    tracing_subscriber::fmt::init();
+
     let app = Headlines::new();
     let mut win_option = NativeOptions::default();
     win_option.initial_window_size = Some(Vec2::new(540., 960.));
+
     run_native(Box::new(app), win_option);
 }
